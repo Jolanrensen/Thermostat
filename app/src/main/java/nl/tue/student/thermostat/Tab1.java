bb24fc542db1e074ed93c0cd7ea53a0dca4ab7de
Jolanrensen <j.j.r.rensen@student.tue.nl>
2016-05-31 20:59:57 +0200

    Initial commit

---
+++ app/src/main/java/nl/tue/student/thermostat/Tab1.java
@@ -0,0 +1,41 @@
+package nl.tue.student.thermostat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Belal on 2/3/2016.
+ */
+
+//Our class extending fragment
+public class Tab1 extends Fragment {
+
+    //Overriden method onCreateView
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1,container,false);
+        Button button = (Button)view.findViewById(R.id.button);
+        final TextView text = (TextView)view.findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (text.getText() == "hallo") {
+                    text.setText("jij");
+                } else {
+                    text.setText("hallo");
+                }
+
+            }
+        });
+
+        //Returning the layout file after inflating
+        //Change R.layout.tab1 in you classes
+        return view;
+    }
+}
