b75c1dd4208b5c8a2cf88f92b5c99d2960dd7c88
Jolanrensen <j.j.r.rensen@student.tue.nl>
2016-06-05 21:57:03 +0200

    Added cards behind listview and arc, changed color scheme, added textview inside the arc that shows its progess

--- app/src/main/java/nl/tue/student/thermostat/Homepage.java
+++ app/src/main/java/nl/tue/student/thermostat/Homepage.java
@@ -1,29 +1,63 @@
 package nl.tue.student.thermostat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.triggertrap.seekarc.SeekArc;
 
-/**
- * Created by Belal on 2/3/2016.
- */
-
 
 public class Homepage extends Fragment {
+    TextView currentTemp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.homepage, container, false);
-        SeekArc seekArc = (SeekArc)view.findViewById(R.id.seekArc);
 
+        //importing current temperature text
+        currentTemp = (TextView)view.findViewById(R.id.currentTemp);
+
+        //importing the arc
+        SeekArc seekArc = (SeekArc)view.findViewById(R.id.seekArc);
+        seekArc.setStartAngle(0);
+        seekArc.setSweepAngle(280);
+        seekArc.setTouchInSide(true);
+        seekArc.setArcWidth(10);
+        seekArc.setArcRotation(220);
+        seekArc.setProgressWidth(40);
+        seekArc.setRoundedEdges(true);
+        seekArc.setProgressColor(Color.parseColor("#448aff"));
+        seekArc.setArcColor(Color.parseColor("#f44336"));
+
+
+
+        seekArc.setOnSeekArcChangeListener(new SeekArc.OnSeekArcChangeListener() {
+            @Override
+            public void onProgressChanged(SeekArc seekArc, int i, boolean b) {
+               currentTemp.setText(Integer.toString(seekArc.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekArc seekArc) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekArc seekArc) {
+
+            }
+        });
+
+        //importing the upcoming changes list
         ListView listview= (ListView) view.findViewById(R.id.upcomingChangesList);
         listview.setAdapter(new CustomListAdapter(this.getContext()));
 
+        currentTemp.setText(Integer.toString(seekArc.getProgress()));
         return view;
     }
 }
