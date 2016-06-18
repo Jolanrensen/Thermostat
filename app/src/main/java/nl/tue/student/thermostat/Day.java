bd86aee91bb131e3b483c0bbb99c79e69f2281bb
Jolanrensen <j.j.r.rensen@student.tue.nl>
2016-06-18 18:25:03 +0200

    done with upcoming changes

--- app/src/main/java/nl/tue/student/thermostat/Day.java
+++ app/src/main/java/nl/tue/student/thermostat/Day.java
@@ -1,21 +1,16 @@
 package nl.tue.student.thermostat;
 
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.MainThread;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.NumberPicker;
-import android.widget.TextView;
 
 import com.quentindommerc.superlistview.SuperListview;
 import com.quentindommerc.superlistview.SwipeDismissListViewTouchListener;
@@ -24,7 +19,6 @@ import org.thermostatapp.util.HeatingSystem;
 import org.thermostatapp.util.Switch;
 import org.thermostatapp.util.WeekProgram;
 
-import java.util.List;
 import java.util.ArrayList;
 
 public class Day extends AppCompatActivity {
@@ -50,6 +44,7 @@ public class Day extends AppCompatActivity {
             day = extras.getString("day");
         }
         toolbar.setTitle(day);
+
         setSupportActionBar(toolbar);
 
         ArrayList<String> lst = new ArrayList<String>();
