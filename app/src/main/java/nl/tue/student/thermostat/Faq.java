17b694b8e0b76bea4a72283932c213567b90f8a6
Jolanrensen <j.j.r.rensen@student.tue.nl>
2016-06-19 16:17:06 +0200

    added faq

---
+++ app/src/main/java/nl/tue/student/thermostat/Faq.java
@@ -0,0 +1,69 @@
+package nl.tue.student.thermostat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Faq extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faq);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        toolbar.setTitle("FAQ");
+        setSupportActionBar(toolbar);
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent parentIntent = NavUtils.getParentActivityIntent(this);
+                if (parentIntent == null) {
+                    finish();
+                    return true;
+                } else {
+                    parentIntent.setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    startActivity(parentIntent);
+                    finish();
+                    return true;
+                }
+            case R.id.help:
+                //do something helpful
+                ;
+                //Toast toast = Toast.makeText(getApplicationContext(), "Not yet implemented", Toast.LENGTH_LONG);
+                //toast.show();
+                for (int i = 0; i < 4; i++) {
+                    Toast.makeText(getApplicationContext(), "Do you seriously need help in a FAQ screen? Well... You can go back to the homepage by pressing the 'back' button.", Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.help_menu, menu);
+        return true;
+    }
+
+}
